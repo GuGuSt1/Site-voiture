5d3d9e5e813d076a81ed76ff16e1bc3379710952
GuGuSt1 <131517593+GuGuSt1@users.noreply.github.com>
2023-04-22 09:48:34 +0200

    Add files via upload

---
+++ MonSite Final/app.js
@@ -0,0 +1,37 @@
+
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+const animals = document.querySelector('.animals');
+
+animals.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
